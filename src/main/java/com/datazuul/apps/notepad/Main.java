4fd6f1afc0dc39c5ca05b4b0fc07911bf61431f2
Ralf Eichinger <ralf.eichinger@gmail.com>
2017-06-19 20:02:16 +0200

    Fix non existing file argument

--- src/main/java/com/datazuul/apps/notepad/Main.java
+++ src/main/java/com/datazuul/apps/notepad/Main.java
@@ -5,23 +5,24 @@ import java.io.File;
 
 public class Main {
 
-    public static final String PROGRAM_NAME = "Notepad";
-    public static final String VERSION = "1.0";
+  public static final String PROGRAM_NAME = "Notepad";
+  public static final String VERSION = "1.0";
 
-    public static void main(String args[]) {
-        MainWindow mainWindow;
+  public static void main(String args[]) {
+    MainWindow mainWindow;
 
-        //	try {
-        //	    String cn = UIManager.getSystemLookAndFeelClassName();
-        //	    UIManager.setLookAndFeel(cn); // Use the native L&F
-        //	} catch (Exception cnf) {
-        //	}
-        if (args.length > 0) {
-            mainWindow = new MainWindow(new File(args[0]));
-        } else {
-            mainWindow = new MainWindow();
-        }
-
-        mainWindow.setVisible(true);
+    //	try {
+    //	    String cn = UIManager.getSystemLookAndFeelClassName();
+    //	    UIManager.setLookAndFeel(cn); // Use the native L&F
+    //	} catch (Exception cnf) {
+    //	}
+    if (args.length > 0) {
+      final File file = new File(args[0]);
+      mainWindow = new MainWindow(file);
+    } else {
+      mainWindow = new MainWindow();
     }
+
+    mainWindow.setVisible(true);
+  }
 }
--- src/main/java/com/datazuul/apps/notepad/backend/FileDaoImpl.java
+++ src/main/java/com/datazuul/apps/notepad/backend/FileDaoImpl.java
@@ -8,44 +8,46 @@ import java.io.IOException;
 
 public class FileDaoImpl implements FileDao {
 
-    public FileDaoImpl() {
-        super();
-    }
+  public FileDaoImpl() {
+    super();
+  }
 
-    @Override
-    public char[] read(File file) {
-        char[] data = null;
+  @Override
+  public char[] read(File file) {
+    char[] data = null;
 
-        FileReader fin = null;
-        try {
-            fin = new FileReader(file);
-            int filesize = (int) file.length();
-            data = new char[filesize];
-            fin.read(data, 0, filesize);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fin.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    FileReader fin = null;
+    try {
+      fin = new FileReader(file);
+      int filesize = (int) file.length();
+      data = new char[filesize];
+      fin.read(data, 0, filesize);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (fin != null) {
+          fin.close();
         }
-
-        return data;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
-    @Override
-    public void save(File file, String text) {
-        try {
-            FileWriter fw = new FileWriter(file);
-            int textsize = text.length();
-            fw.write(text, 0, textsize);
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    return data;
+  }
+
+  @Override
+  public void save(File file, String text) {
+    try {
+      FileWriter fw = new FileWriter(file);
+      int textsize = text.length();
+      fw.write(text, 0, textsize);
+      fw.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
